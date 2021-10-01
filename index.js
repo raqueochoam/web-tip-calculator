250a6f956f4a88ec56e9a26ad95f06df73606e87
raqueochoam <raqueochoam@gmail.com>
2021-10-01 09:15:09 -0500

    01/10/2021

---
+++ index.js
@@ -0,0 +1,27 @@
+var isPress;
+var button = document.getElementById("tip");
+
+function selectTip(x,id){
+    if(button.value==false){
+        isPres=true;
+        console.log(isPress);
+    }
+    else{
+        isPress=false;
+        console.log(isPress);
+    }
+    var tip = x;
+    console.log(tip);
+}
+
+function calculateTipAmount(){
+    var bill = document.getElementById("bill").value;
+    var people = document.getElementById("people").value;
+
+
+}
+
+function calculateTotal(){
+
+}
+
